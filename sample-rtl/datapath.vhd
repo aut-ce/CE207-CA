7cb1f3c9f9180b610ebaa7f103796e5062f93762
Parham Alvani <parham.alvani@gmail.com>
2017-03-06 19:56:44 +0330

    datapath module was added.

--- sample-rtl/datapath.vhd
+++ sample-rtl/datapath.vhd
@@ -8,18 +8,18 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity datapath is
-	port (register_en : in std_logic;
+	port (register_load, register_shift : in std_logic;
 		clk : in std_logic;
-		register_out : out std_logic_vector (3 downto 0);
+		register_out : out std_logic;
 		register_in : in std_logic_vector (3 downto 0));
 end entity;
 
 architecture rtl of datapath is
 	component four_register is
 		port (d : in std_logic_vector(3 downto 0);
-			clk, en : in std_logic;
-			q : out std_logic_vector(3 downto 0));
+			clk, load, shift : in std_logic;
+			qout : out std_logic);
 	end component;
 begin
-	r: four_register port map (register_in, clk, register_en, register_out);
+	r: four_register port map (register_in, clk, register_load, register_shift, register_out);
 end architecture;
