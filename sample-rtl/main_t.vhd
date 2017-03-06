2111e69f828c556e9e5ebc018e06a236b217e3b9
Parham Alvani <parham.alvani@gmail.com>
2017-03-06 19:59:41 +0330

    Main module test bench was added.

--- sample-rtl/main_t.vhd
+++ sample-rtl/main_t.vhd
@@ -14,11 +14,12 @@ architecture rtl of main_t is
 	component main
 		port (clk, rst : in std_logic;
 			data_in : in std_logic_vector(3 downto 0);
-			data_out : out std_logic_vector(3 downto 0));
+			data_out : out std_logic);
 	end component;
 
 	signal clk, reset : std_logic := '0';
-	signal data_out, data_in : std_logic_vector(3 downto 0);
+	signal data_in : std_logic_vector(3 downto 0);
+	signal data_out : std_logic;
 begin
 	m : main port map (clk, reset, data_in, data_out);
 	reset <= '1', '0' after 10 ns;
