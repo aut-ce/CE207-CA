e6adea18f51950691867556e1106a208e38c479a
Parham Alvani <parham.alvani@gmail.com>
2017-03-06 19:36:57 +0330

    Register with shift support was added.

--- sample-rtl/register.vhd
+++ sample-rtl/register.vhd
@@ -9,17 +9,21 @@ use IEEE.std_logic_1164.all;
 
 entity four_register is
 	port (d : in std_logic_vector(3 downto 0);
-		clk, en : in std_logic;
-		q : out std_logic_vector(3 downto 0));
+		clk, load, shift : in std_logic;
+		qout : out std_logic);
 end entity;
 
 architecture behavior of four_register is
+	signal q : std_logic_vector(3 downto 0);
 begin
 	process (clk)
 	begin
 		if clk = '1' and clk'event then
-			if en = '1' then
+			if load = '1' then
 				q <= d;
+			elsif shift='1' then
+			  qout <= q(3);
+			  q <= q(2 downto 0) & '0';
 			end if;
 		end if;
 	end process;
